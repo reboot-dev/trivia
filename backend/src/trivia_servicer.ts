398f4d91f5a1f137202dd5e6cb32feddab961cbb
Stu Hood <stuhood@gmail.com>
2025-02-11 13:14:13 -0800

    Initialize the question.

--- backend/src/trivia_servicer.ts
+++ backend/src/trivia_servicer.ts
@@ -4,6 +4,8 @@ import {
 } from "@reboot-dev/reboot";
 import {
   Game,
+  GameStatus,
+  Question,
   AddPlayerRequest,
   AddPlayerResponse,
 } from "../../api/trivia/v1/trivia_rbt.js";
@@ -15,6 +17,25 @@ export class GameServicer extends Game.Servicer {
     request: AddPlayerRequest
   ): Promise<AddPlayerResponse | PartialMessage<AddPlayerResponse>> {
     state.players.push(request.playerName);
+
+    if (state.players.length === 1) {
+      await nextQuestion(state);
+    }
+
     return {};
   }
+}
+
+function nextQuestion(state: Game.State) {
+  state.status = GameStatus.QUESTION_RUNNING;
+
+  state.question = new Question({
+    question: "What is your favorite color?",
+    potentialAnswers: [
+      "red",
+      "blue",
+      "green",
+      "pink",
+    ],
+  });
 }
